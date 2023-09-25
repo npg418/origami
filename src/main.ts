a143afa584857dc726603926eede01a70fe465c9
NPG418 <narupenguin919@gmail.com>
2023-09-25 21:33:20 +0900

    折るアニメーションの追加

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import './style.css';
-import OrigamiPaper from './paper/paper';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -10,26 +9,62 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xefefef);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(0, 0, 300);
 
-const crane = new OrigamiPaper(100);
-const material = new THREE.MeshLambertMaterial({ color: 0xff0000, wireframe: true });
-const mesh = new THREE.Mesh(crane, material);
-mesh.position.set(0, 0, 0);
-scene.add(mesh);
+const group = new THREE.Group();
+
+const geometry = new THREE.PlaneGeometry(100, 100);
+const material = new THREE.MeshLambertMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+const mesh = new THREE.Mesh(geometry, material);
+group.add(mesh);
+
+const wireMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true });
+const line = new THREE.Mesh(geometry, wireMaterial);
+group.add(line);
+
+scene.add(group);
+
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.set(100, 100, 100);
+scene.add(light);
+
+const ambient = new THREE.AmbientLight(0xfcfcfc);
+scene.add(ambient);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-const light = new THREE.DirectionalLight();
-light.position.set(0, 0, 500);
-scene.add(light);
+function fold(deg: number) {
+  const op = Math.sqrt(2) * 50;
+  const z = Math.sin((deg * Math.PI) / 180) * op;
+  const d = Math.cos((deg * Math.PI) / 180) * op;
+  const dx = Math.sin((1 / 4) * Math.PI) * d;
+  const dy = Math.cos((1 / 4) * Math.PI) * d;
 
+  group.children.forEach((mesh) => {
+    if (mesh instanceof THREE.Mesh) {
+      const pos = mesh.geometry.attributes.position;
+      pos.setXYZ(3, dx, -dy, z);
+      pos.needsUpdate = true;
+    }
+  });
+}
+
+let deg = 0;
+let revert = false;
 function animate() {
   requestAnimationFrame(animate);
 
   controls.update();
+
+  revert ? deg-- : deg++;
+  if (deg >= 180) {
+    revert = true;
+  } else if (deg <= 0) {
+    revert = false;
+  }
+  fold(deg);
 
   renderer.render(scene, camera);
 }
--- src/paper/step2vertex.ts
+++ src/paper/step2vertex.ts
@@ -1,12 +1,12 @@
 type FoldingStep =
   | {
-    from: [number, number];
-    to: [number, number];
-    type: 'mountain' | 'valley';
-  }
+      from: [number, number];
+      to: [number, number];
+      type: 'mountain' | 'valley';
+    }
   | {
-    type: 'flip';
-  };
+      type: 'flip';
+    };
 
 export type FoldingSteps = FoldingStep[];
 
@@ -19,64 +19,53 @@ export default function getVertex(steps: FoldingSteps) {
   const vertex: Point[] = [];
   for (const step of steps) {
     if (step.type === 'flip') continue;
-    const {
-      from,
-      to,
-    } = step;
+    const { from, to } = step;
     vertex.push(from, to);
-    prevLines.forEach((line) => {
-      if (doesIntersect(line, [from, to])) {
-        const pf = symmetricPoint(from, line);
-        const pt = symmetricPoint(to, line);
-        vertex.push(pf, pt);
-      }
-    });
-    prevLines.push([
-      from,
-      to,
-    ]);
+    prevLines.push([from, to]);
   }
 }
 
-function getInterSection([[xa1, ya1], [xa2, ya2]]: Line, [[xb1, yb1], [xb2, yb2]]: Line) {
-  const dx1 = xa2 - xa1;
-  const dy1 = ya2 - ya1;
-  const dx2 = xb2 - xb1;
-  const dy2 = yb2 - yb1;
-  const det = dx1 * dy2 - dx2 * dy1;
-  if (det === 0) return null;
-  const dx3 = xb1 - xa1;
-  const dy3 = yb1 - ya1;
-  const t = (dx3 * dy2 - dx2 * dy3) / det;
-  const u = (dx1 * dy3 - dx3 * dy1) / det;
-  if (t < 0 || t > 1 || u < 0 || u > 1) return null;
-  return [
-    xa1 + t * dx1,
-    ya1 + t * dy1,
-  ];
-}
+function getCrossPoint([[xa1, ya1], [xa2, ya2]]: Line, [[xb1, yb1], [xb2, yb2]]: Line) {
+  const inclineA = (ya2 - ya1) / (xa2 - xa1);
+  const inclineB = (yb2 - yb1) / (xb2 - xb1);
 
+  const interceptA = ya1 - inclineA * xa1;
+  const interceptB = yb1 - inclineB * xb1;
+
+  const x = (interceptB - interceptA) / (inclineA - inclineB);
+  const y = inclineA * x + interceptA;
+
+  return [x, y];
+}
 
 function symmetricPoint([xp, yp]: Point, [[x1, y1], [x2, y2]]: Line) {
   const incline = (y2 - y1) / (x2 - x1);
   const lineFunc = (x: number) => incline * (x - x1) + y1;
-
   const xp$ = xp + 1;
   const yp$ = lineFunc(xp$);
-
-  const intersection = getInterSection([
-    [xp, yp],
-    [xp$, yp$],
-  ], [
-    [x1, y1],
-    [x2, y2],
-  ]);
-
-  if (!intersection) return null;
-  const [ix, iy] = intersection;
-
-  const x = 2 * ix - xp;
-  const y = 2 * iy - yp;
-
+  const crossPoint = getCrossPoint(
+    [
+      [xp, yp],
+      [xp$, yp$],
+    ],
+    [
+      [x1, y1],
+      [x2, y2],
+    ]
+  );
+  if (!crossPoint) return null;
+  const [xc, yc] = crossPoint;
+  const x = 2 * xc - xp;
+  const y = 2 * yc - yp;
   return [x, y];
 }
+
+console.log(
+  symmetricPoint(
+    [0, 3],
+    [
+      [0, 0],
+      [1, 1],
+    ]
+  )
+);
