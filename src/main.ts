5e5a5f270721e31cd37822f8238d111c855734f1
NPG418 <narupenguin919@gmail.com>
2023-11-07 20:34:10 +0900

    staticじゃなくてpublicだそうです

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ camera.position.set(0, 0, 300);
 
 const gltfLoader = new GLTFLoader();
 let mixer: THREE.AnimationMixer;
-gltfLoader.load('/static/car/01.glb', (gltf) => {
+gltfLoader.load('/public/car/01.glb', (gltf) => {
   scene.add(gltf.scene);
   mixer = new THREE.AnimationMixer(gltf.scene);
   gltf.animations.forEach((animation) => {
