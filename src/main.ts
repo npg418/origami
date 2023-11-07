7e5317191655915e719ba32949b31a35c70e7bfc
NPG418 <narupenguin919@gmail.com>
2023-11-07 20:18:44 +0900

    blenderのアニメーションを表示してみる

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 import './style.css';
-import OrigamiPaper from './paper/paper';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -10,26 +10,36 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xefefef);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.set(0, 0, 300);
 
-const crane = new OrigamiPaper(100);
-const material = new THREE.MeshLambertMaterial({ color: 0xff0000, wireframe: true });
-const mesh = new THREE.Mesh(crane, material);
-mesh.position.set(0, 0, 0);
-scene.add(mesh);
+const gltfLoader = new GLTFLoader();
+let mixer: THREE.AnimationMixer;
+gltfLoader.load('/static/car/01.glb', (gltf) => {
+  scene.add(gltf.scene);
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  gltf.animations.forEach((animation) => {
+    mixer.clipAction(animation).play();
+  });
+});
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
 const light = new THREE.DirectionalLight();
-light.position.set(0, 0, 500);
+light.position.set(0, 500, 500);
 scene.add(light);
 
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const clock = new THREE.Clock();
 function animate() {
   requestAnimationFrame(animate);
 
   controls.update();
+
+  mixer?.update(clock.getDelta());
 
   renderer.render(scene, camera);
 }
